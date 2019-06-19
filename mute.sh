72ce8eeaa42b84d0fd182dcdca8ae6855bf40caa
0x0J <52012157+0x0J@users.noreply.github.com>
2019-06-19 23:13:56 +0200

    Add files via upload

---
+++ mute.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+
+state=`amixer -D pulse set Capture toggle | gawk 'match($0, /(Front Left|Mono).*\[(.*)\]/, a) {print a[2]}'`
+if [ "$state" = "off" ]; then
+    icon="audio-input-microphone-muted-symbolic"
+else
+    icon="audio-input-microphone-symbolic"
+fi
+notify-send --hint=int:transient:1 -i $icon "Mic switched: $state"
+
